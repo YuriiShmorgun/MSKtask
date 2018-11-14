a94afe642391aa6a5f6aa14058559d53c477dc5e
YuriiSH <yurashmorgun@gmail.com>
2018-11-14 23:55:33 +0200

    +

--- com/msk/java/src/model/entity/Room.java
+++ com/msk/java/src/model/entity/Room.java
@@ -1,5 +1,14 @@
 package model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
 public class Room {
 
     private Integer width;
