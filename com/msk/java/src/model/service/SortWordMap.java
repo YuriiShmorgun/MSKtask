4cdb26e5ae128bac57c2c9c92f43bafd214551a7
YuriiSH <yurashmorgun@gmail.com>
2018-11-15 19:01:43 +0200

    DONE+

--- com/msk/java/src/model/service/SortWordMap.java
+++ com/msk/java/src/model/service/SortWordMap.java
@@ -13,15 +13,11 @@ public class SortWordMap {
         Map <String, String> sortMap = new TreeMap<>(Comparator.naturalOrder() );
         String temp;
         String temp2;
-
         for (Map.Entry<String , Integer> entry: map.entrySet()){
-
             temp =((Integer.toString(Integer.MAX_VALUE - entry.getValue())).concat(entry.getKey()));
             temp2 = (entry.getKey() + " = " + (entry.getValue()));
             sortMap.put(temp, temp2);
         }
-
-
         return sortMap;
     }
 }
